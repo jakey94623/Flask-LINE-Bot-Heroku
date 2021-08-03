c3ae9a5719be0cd474fab6feca447d04af0f4d8a
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2021-08-03 12:21:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 import psycopg2
 
-conn = psycopg2.connect(database="testdb", user="postgres", password="pass123", host="127.0.0.1", port="5432")
+conn = psycopg2.connect(database="dcd5jca9btqeoi", user="
+azmkghqpoeannh", password="9a117f2c22ec39525492ef4c21c9c6d09ce2a3758336b3fa6581c87b564980b6", host="ec2-52-1-20-236.compute-1.amazonaws.com", port="5432")
 
 print ("Opened database successfully")
 
