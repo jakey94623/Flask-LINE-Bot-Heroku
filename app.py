b50d4b14bb4524c885c5ff8cffc5303875d6f2c2
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2021-09-29 15:21:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,8 +55,9 @@ def handle_message(event):
     if userid=="U533d128e64adb1462e59acb57b2929d8":
             get_message = "哈囉~璇老大"
     elif userid=="U3c822c99099ebc65694c3b8401be9707":
+        textInt = random.randint(1, 9);
         if 1 == 1:
-            get_message = "你最棒了，老鐵"
+            get_message = textInt
         if 1 == 2:
              get_message = "你最棒了!老鐵"
     elif userid=="U747ea245f9be60ab55edb6c9d9388d38":
