55168915ff11b1dd62fd6d3e7a28751944912f05
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2021-09-29 14:59:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def handle_message(event):
         conn.close();
     except DatabaseError:
         transaction.rollback()
-    if userid=="U533d128e64adb1462e59acb57b2929d8" :
+    if userid=="U533d128e64adb1462e59acb57b2929d8":
          textInt = random.randint(1, 5);
         if textInt == 1:
             get_message = "哈囉~璇老大"
@@ -64,7 +64,7 @@ def handle_message(event):
             get_message = "等一下! 大家現在可以看一下詩璇嗎 是不是變好看了呀"
         if textInt == 5:
             get_message = "恭喜發財"
-    elif userid=="U3c822c99099ebc65694c3b8401be9707" :
+    elif userid=="U3c822c99099ebc65694c3b8401be9707":
         textInt = random.randint(1, 5);
         if textInt == 1:
             get_message = "不要再吃了"
@@ -76,7 +76,7 @@ def handle_message(event):
             get_message = "晚餐該吃個好的吧😎"
         if textInt == 5:
             get_message = "YO~"
-    elif userid=="U747ea245f9be60ab55edb6c9d9388d38" :
+    elif userid=="U747ea245f9be60ab55edb6c9d9388d38":
         textInt = random.randint(1, 5);
         if textInt == 1:
             get_message = "嗨伊藤"
